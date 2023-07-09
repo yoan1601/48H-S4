63268f8758ee7fece8164f40960e8d205c6d709d
Yoan <yoan.rab@gmail.com>
2023-07-09 23:15:24 +0200

    1st commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,23 @@
+<!-- main-panel ends -->
+</div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src=<?php echo site_url('assets/vendors/js/vendor.bundle.base.js'); ?>"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src=<?php echo site_url('assets/vendors/chart.js/Chart.min.js'); ?>"></script>
+    <script src=<?php echo site_url('assets/js/jquery.cookie.js'); ?>" type="text/javascript"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src=<?php echo site_url('assets/js/off-canvas.js'); ?>"></script>
+    <script src=<?php echo site_url('assets/js/hoverable-collapse.js'); ?>"></script>
+    <script src=<?php echo site_url('assets/js/misc.js'); ?>"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <script src=<?php echo site_url('assets/js/dashboard.js'); ?>"></script>
+    <script src=<?php echo site_url('assets/js/todolist.js'); ?>"></script>
+    <!-- End custom js for this page -->
+  </body>
+</html>
